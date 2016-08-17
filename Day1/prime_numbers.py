9b74ea3c59fe95d75909fa394914a1171fb61ff7
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-08-17 08:35:32 +0300

    Adds comments to Prime Numbers function

--- Day1/prime_numbers.py
+++ Day1/prime_numbers.py
@@ -1,9 +1,13 @@
 def Get_Primes_Upto(x):
     prime_numbers = [2, 3]
-    start_point = 4
 
+    '''Start by initializing the list with 2 and 3 as the first prime numbers'''
+
+    start_point = 4
+    # Start at 4
     while True:
         for number in range(2, int(start_point**0.5) + 1):
+            # check upto the square root, update from the from half
 
             if start_point % number == 0:
                 break
@@ -13,6 +17,7 @@ def Get_Primes_Upto(x):
         start_point += 1
         if start_point == x + 1:
             break
+    # return as a string
     return str(prime_numbers)
-#Testing the function
+# Testing the function
 print(Get_Primes_Upto(11))
